8df2bb135849b327fbe9b5b1c6b5968f17048b53
ADARSH-IIIT <adarshtech251@gmail.com>
2024-03-10 15:54:07 +0530

    updated how to print something on console

---
+++ print_output.cpp
@@ -0,0 +1,73 @@
+// important facts
+// no need to return statement in case of int main , compiler of c++ automatically insert return statement by itself at the end line of code
+// it is not neccassry to return 0 , we can return any value , even negative values also
+///   sirf cout use karunga line by line toh sabhi string same line mein hi rpint hongi 
+/////    only count is line print ,  it wont act as println
+
+
+
+
+
+////////////////////////////////////////////////////   basic program ///////////////
+// #include <iostream>
+
+// int main() {
+
+//     std::cout << "Hello, World!";
+//     return 0;
+// }
+
+
+
+
+
+
+
+// #include <iostream>
+// using namespace std ;
+
+
+// int main() {
+//     cout << "Hello, World!";
+//     return 5;
+// }
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+//////////////////////////////// how to print multiple strings in new and same line
+
+// endl  ==> end line and do next things from new line 
+
+// \n can also be used 
+
+// #include <iostream>
+
+// using namespace std ;
+
+
+// int main(){
+
+
+
+//     cout << "hello man \n my name is adarsh"  ;
+
+//     cout << "hello man " << "my name is adarsh" ; 
+    
+//     cout << "hello man "  <<  endl <<  "my name is adarsh"    ;
+
+//     return 0 ;
+// }
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+////////////////////////   printing var in console ///////////////////////
+
+// cout << "valuie od variable is "  << varnamehre << "next string here"
+
+//   cout << firstvar << secondvar << thirdvar << and so on   ; 
+/////////////////////////////////////////////////////////////////////////////////////////////
